fc684af9948b69163b6033fc3473fcf811fc0c09
saik64757 <saik64757@gmail.com>
2023-03-05 15:43:25 +0530

    after adding Logo comp

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <img
+      src={require("../../Assetes/Logo qtify.png")}
+      alt="logo"
+      width={"67px"}
+    />
+  );
+}
+
+export default Logo;
