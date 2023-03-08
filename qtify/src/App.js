30f4e9d81c7c8d263a5adaf4e1efef89cebb46a0
saik64757 <saik64757@gmail.com>
2023-03-08 16:01:08 +0530

    card-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Card />
     </>
   );
 }
---
+++ qtify/src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+function Card() {
+  return (
+    <>
+      <div className={styles.CardWrapper}>
+        <div className={styles.cardContent}>
+          <img src={require("../../Assetes/Cardimage.png")} alt="card image" />
+          <p>100M Follows</p>
+        </div>
+        <p>New BollyWood</p>
+      </div>
+    </>
+  );
+}
+
+export default Card;
