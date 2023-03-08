bb67604daf83b0476a61828d84cfd971a63a2f88
saik64757 <saik64757@gmail.com>
2023-03-08 14:31:42 +0530

    Commiting nav bar

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Logo from "../Logo/Logo";
 import Searchbar from "../Searchbar/Searchbar";
 import styles from "./Navbar.module.css";
+import Button from "../Button/Button";
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
       <Logo />
       <Searchbar />
-      <button className={styles.feedBackButton}>Give Feedback</button>
+      <Button />
     </div>
   );
 }
--- qtify/src/components/Searchbar/Searchbar.jsx
+++ qtify/src/components/Searchbar/Searchbar.jsx
@@ -4,23 +4,18 @@ import styles from "./Searchbar.module.css";
 function Searchbar() {
   return (
     <>
-      <div className={styles.Serchbar}>
-        <label htmlFor="header-search">
-          {/* <span className="visually-hidden">Search songs posts</span> */}
-        </label>
+      <div className={styles.Serchwrap}>
         <input
-          className={styles.serchinput}
           type="text"
-          id="header-search"
           placeholder="Search a song of your choice"
-          name="s"
+          className={styles.serchinput}
         />
-        <button className={styles.serchButton}>Serch</button>
+        <button className={styles.serchButton}>
+          <img src={require("../../Assetes/Searchicon.png")} alt="" />
+        </button>
       </div>
     </>
   );
 }
 
 export default Searchbar;
-
-// className={styles.serchButton}
