10c38b1a055618d8c224d1a5aba4eb38473d5d9b
saik64757 <saik64757@gmail.com>
2023-06-27 22:26:01 +0530

    AudioPlayerSection Done

--- qtify/src/components/SongController/SongController.jsx
+++ qtify/src/components/SongController/SongController.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SongController.module.css";
 
 function SongController() {
+  const [isPlaying, setisPlaying] = useState(true);
   return (
     <>
       <div className={styles.songControllerWrapper}>
@@ -15,6 +16,39 @@ function SongController() {
           <div>
             <p className={styles.SongName}>Song name</p>
             <p className={styles.SongAlbum}>Album name</p>
+          </div>
+        </div>
+        <div className={styles.songPlayer}>
+          {isPlaying ? (
+            <button
+              onClick={() => {
+                setisPlaying((prevValue) => !prevValue);
+              }}
+            >
+              <img
+                src={require("../../Assetes/Frame 3748.png")}
+                alt=""
+                className={styles.playPauseButton}
+              />
+            </button>
+          ) : (
+            <button
+              className={styles.playPauseButton}
+              onClick={() => {
+                setisPlaying((prevValue) => !prevValue);
+              }}
+            >
+              <img src={require("../../Assetes/pauseButton.png")} alt="" />
+            </button>
+          )}
+          <div className={styles.playerTime}>
+            <span>0:08</span>
+            <img
+              src={require("../../Assetes/Group 3745.png")}
+              alt=""
+              className={styles.audioPlayer}
+            />
+            <span>3:08</span>
           </div>
         </div>
       </div>
